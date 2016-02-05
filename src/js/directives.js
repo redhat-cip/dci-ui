4d19105bd519ecdd87e490a8a4efa21a1ff8a66c
Maxime Vidori <maxime.vidori@gmail.com>
2016-02-05 04:37:37 -0900

    Fix duration calculation error on job running
    Change-Id: I15487f6367e1d0647e33f36fdc4d4d9dbb3a137b

--- src/js/directives.js
+++ src/js/directives.js
@@ -26,7 +26,8 @@ require('app')
 
       var job = scope.job;
       var start = moment(job.created_at);
-      job.time_running = moment(job.updated_at).to(job.created_at, true);
+      job.time_running = (moment(job.updated_at)
+                          .to(moment.moment(job.created_at), true));
       job.updated_at = moment(job.updated_at).from(moment.moment());
 
       job.glyphicon = status[job.status].glyphicon;
