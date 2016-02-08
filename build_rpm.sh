361ea25b74320eaa2d3737af66fb51b84c189078
Yanis Guenane <yguenane@redhat.com>
2016-02-08 09:55:38 +0100

    packaging: Sign our packaged during build process
    This commit aims to sign our packaged during the build process.
    We rely on the sign[1] plugin offered by mock to run the rpm --addsign
    command. In order to sign them we need to specify two custom rpm macros
    that are :
    
      * %_signature
      * %_gpg_name
    
    Finally we wrap everything around expect so it is non-blocking
    
    [1]
    https://fedoraproject.org/wiki/Mock/Plugin/Sign?rd=Subprojects/Mock/Plugin/Sign
    
    Change-Id: Icabaafd595db6e391929f1a3e9f0390ae266c447

--- build_rpm.sh
+++ build_rpm.sh
@@ -4,6 +4,11 @@ PROJ_NAME=dci-ui
 DATE=$(date +%Y%m%d%H%M)
 SHA=$(git rev-parse HEAD | cut -c1-8)
 
+# Configure rpmmacros to enable signing packages
+#
+echo '%_signature gpg' >> ~/.rpmmacros
+echo '%_gpg_name Distributed-CI' >> ~/.rpmmacros
+
 # Create the proper filesystem hierarchy to proceed with srpm creatioon
 #
 rm -rf ${HOME}/rpmbuild
@@ -16,6 +21,16 @@ rpmbuild -bs ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
 # Build the RPMs in a clean chroot environment with mock to detect missing
 # BuildRequires lines.
 arch=epel-7-x86_64
+
+# NOTE(spredzy) Add signing options
+#
+mkdir -p ${HOME}/.mock
+cp /etc/mock/${arch}.cfg ${HOME}/.mock/${arch}.cfg
+sed -i "\$aconfig_opts['plugin_conf']['sign_enable'] = True" ${HOME}/.mock/${arch}.cfg
+sed -i "\$aconfig_opts['plugin_conf']['sign_opts'] = {}" ${HOME}/.mock/${arch}.cfg
+sed -i "\$aconfig_opts['plugin_conf']['sign_opts']['cmd'] = 'rpmsign'" ${HOME}/.mock/${arch}.cfg
+sed -i "\$aconfig_opts['plugin_conf']['sign_opts']['opts'] = '--addsign %(rpms)s'" ${HOME}/.mock/${arch}.cfg
+
 RPATH='el/7/x86_64'
 mkdir -p development
-mock -r $arch rebuild --resultdir=development/${RPATH} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
+mock -r ${HOME}/.mock/${arch}.cfg rebuild --resultdir=development/${RPATH} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
