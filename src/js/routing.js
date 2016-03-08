af2066747dab45d56436dda2e5681968f2fc3c78
Maxime Vidori <maxime.vidori@gmail.com>
2016-03-08 15:22:35 +0100

    Add a messages services
    This service allows the user to send messages to the UI,
    those messages will be displayed in a dedicated part of the
    interface.
    Messages can be closed by hand or they will fade after a
    configurable time.
    This patch removes all the use of custom alerts mecanisms
    in the code.
    This patch also fix the 404 errors when trying to reach
    an unexisting job, and redirect to the index.
    
    Change-Id: I430c0a5995e278c0bfaf77e0f2a9363bb9050a23

--- src/js/routing.js
+++ src/js/routing.js
@@ -102,8 +102,16 @@ require('app')
       controller: 'JobCtrl',
       templateUrl: '/partials/job.html',
       resolve: {
-        job: ['$stateParams', 'api', 'conf', function($stateParams, api, _) {
-          return api.getJob($stateParams.id);
+        job: ['$injector', '$stateParams', 'conf', function($injector, $stateParams) {
+          var api = $injector.get('api');
+
+          return api.getJob($stateParams.id).catch(function(err) {
+              $injector.get('$state').go('index');
+              $injector.get('messages').alert(
+                err.data && err.data.message || 'Something went wrong', 'danger'
+              );
+            }
+          );
         }]
       }
     })
