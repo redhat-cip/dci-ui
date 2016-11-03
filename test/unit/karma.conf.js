537301dee39a119c83b00f5bb424d91740d74a8c
Guillaume Vincent <guillaume@oslab.fr>
2016-11-03 12:30:08 +0100

    add first unit test with karma
    setup karma with phantomjs running loginCtrl test
    
    Change-Id: I43b2318aa9da07e6657795fb279d651d3e9b4f97

---
+++ test/unit/karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '../..',
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'static/js/app.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'test/unit/**/*.spec.js'
+    ],
+    exclude: [],
+    preprocessors: {},
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    concurrency: Infinity
+  });
+};
