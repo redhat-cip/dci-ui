fc7e08b5629fc58f389736278f6b48be7c6350b7
Guillaume Vincent <gvincent@redhat.com>
2017-02-28 16:51:19 +0100

    Token for remoteci
    display token in remotecis list
    add refresh token button in remoteci edit page
    
    Depends-On: I946535128d33baecf2c60f334232dd4fea9b0c16
    Change-Id: I54390ea88a9dfb97c4d0dbba4fc6c75bd449cb5c

---
+++ src/js/controllers/admin/RemoteCIsCtrl.js
@@ -0,0 +1,64 @@
+// Copyright 2015 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+'use strict';
+
+require('app')
+  .controller('RemoteCIsCtrl', [
+    '$scope', '$state', 'remoteci', 'api', 'messages',
+    function($scope, $state, remoteci, api, messages) {
+      remoteci.data = angular.toJson(remoteci.data, true);
+      $scope.remoteci = remoteci;
+
+      $scope.remoteciForm = {};
+
+      $scope.cancel = _.partial($state.go, 'administrate.remotecis');
+
+      $scope.showError = function(form, field) {
+        return field.$invalid && (field.$dirty || form.$submitted);
+      };
+
+      $scope.update = function() {
+        if ($scope.remoteciForm.$invalid) {
+          return;
+        }
+        var newRemoteCi = _.assign({}, $scope.remoteci, {'data': angular.fromJson($scope.remoteci.data)});
+        api['remotecis'].update(newRemoteCi)
+          .then(function() {
+            messages.alert('Team has been updated', 'success');
+            $state.go('administrate.remotecis');
+          })
+          .catch(function(err) {
+            messages.alert(
+              err.data && err.data.message ||
+              'Something went wrong', 'danger'
+            );
+          });
+      };
+
+      $scope.refreshToken = function(){
+        api['remotecis'].refreshToken($scope.remoteci)
+          .then(function(token) {
+            $scope.remoteci.token = token;
+            messages.alert('Api token has been updated', 'success');
+          })
+          .catch(function(err) {
+            messages.alert(
+              err.data && err.data.message ||
+              'Something went wrong', 'danger'
+            );
+          });
+      }
+    }
+  ]);
