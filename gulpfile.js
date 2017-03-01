b9c0178f4e024a2a32ef4455c94a8ddabcb23187
Guillaume Vincent <gvincent@redhat.com>
2017-03-01 14:14:09 +0100

    minify js ro reduce bundle size
    Change-Id: I813c77b3c6212eb493d3a33db70b42714bed6ffb

--- gulpfile.js
+++ gulpfile.js
@@ -75,6 +75,7 @@ gulp.task('js', function() {
     .pipe(source('app.js'))
     .pipe(buffer())
     .pipe($.sourcemaps.init({loadMaps: true}))
+    .pipe($.uglify())
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest(DIST + '/js/'));
 
