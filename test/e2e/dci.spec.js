60493f11778f0d9dc730d69b390a19a3d458f08b
Guillaume Vincent <gvincent@redhat.com>
2017-04-19 11:03:19 +0200

    Fix inconsistency between local tests and continuous integration tests
     * rewrite end to end tests
     * rewrite and simplify gulp
     * add more end to end tests
    
    Change-Id: I6569015b6ecbffa1bc8c3268b9292b0353d5b5dd

--- test/e2e/dci.spec.js
+++ test/e2e/dci.spec.js
@@ -14,19 +14,98 @@
 
 'use strict';
 
-describe('DCI', function() {
-  beforeAll(function() {
-    var cookie = JSON.stringify({
-      status: 2,
-      team: {name: 'admin'},
-      token: Buffer('admin:admin', 'binary').toString('base64')
-    });
-    browser.manage().addCookie({
-      name: 'user',
-      value: encodeURIComponent(cookie),
-      path: '/',
-      domain: '127.0.0.1'
-    });
+describe('When a user opens DCI', function() {
+  var originalTimeout;
+
+  beforeEach(function() {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+  });
+
+  it('should be redirected to login page', function() {
+    browser.get('/');
+    expect(browser.getCurrentUrl()).toBe('http://127.0.0.1:8000/#!/login');
+  });
+
+  it('should not have console errors on login page', function() {
+    browser.get('/#!/login');
+    browser.findElement(by.id('inputUsername')).sendKeys('admin');
+    browser.findElement(by.id('inputPassword')).sendKeys('admin');
+    browser.findElement(by.id('logInButton')).click();
+    browser.waitForAngular();
+  });
+
+  it('should be possible to recheck a job', function() {
+    element.all(by.css('.job__link')).first().click();
+    expect(browser.getCurrentUrl()).toMatch('/jobs/[a-z0-9-]+/results$');
+  });
+
+  it('should test Logs tab', function() {
+    element(by.xpath("//li//a[text()='Logs']")).click();
+  });
+
+  it('should test Details tab', function() {
+    element(by.xpath("//li//a[text()='Details']")).click();
+  });
+
+  it('should test Edit job tab', function() {
+    element(by.xpath("//li//a[text()='Edit job']")).click();
+  });
+
+  it('should test Context job tab', function() {
+    element(by.xpath("//li//a[text()='Context']")).click();
+  });
+
+  it('should test Stack Details job tab', function() {
+    element(by.xpath("//li//a[text()='Stack Details']")).click();
+  });
+
+  it('should test Files job tab', function() {
+    element(by.partialLinkText("Issues")).click();
+  });
+
+  it('should test Files job tab', function() {
+    element(by.partialLinkText("Files")).click();
+  });
+
+  it('visit /job-definitions', function() {
+    browser.get('/#!/job-definitions');
+    browser.waitForAngular();
+  });
+
+  it('visit /logs', function() {
+    browser.get('/#!/logs');
+    browser.waitForAngular();
+  });
+
+  it('visit /information', function() {
+    browser.get('/#!/information');
+    browser.waitForAngular();
+  });
+
+  it('visit /gpanel', function() {
+    browser.get('/#!/gpanel');
+    browser.waitForAngular();
+  });
+
+  it('visit /admin/users', function() {
+    browser.get('/#!/admin/users');
+    browser.waitForAngular();
+  });
+
+  it('visit /admin/teams', function() {
+    browser.get('/#!/admin/teams');
+    browser.waitForAngular();
+  });
+
+  it('visit /admin/topics', function() {
+    browser.get('/#!/admin/topics');
+    browser.waitForAngular();
+  });
+
+  it('visit /admin/remotecis', function() {
+    browser.get('/#!/admin/remotecis');
+    browser.waitForAngular();
   });
 
   afterEach(function() {
@@ -34,61 +113,10 @@ describe('DCI', function() {
       expect(browserLog.length).toEqual(0,
         'there is an error in console.log:\n' + JSON.stringify(browserLog, null, 2));
     });
+
   });
 
-  it('should be possible to recheck a job', function() {
-    browser.get('/');
-    element.all(by.css('.btn-recheck:not([disabled])')).first().click();
-    expect(browser.getCurrentUrl()).toMatch('/jobs/[a-z0-9-]+/results$');
-  });
-
-  it('should be possible to view job details', function() {
-    browser.get('/');
-    element.all(by.css('a.title')).first().click();
-    expect(browser.getCurrentUrl()).toMatch('/jobs/[a-z0-9-]+/results$');
-  });
-
-  it('should be able to search for a job', function() {
-    browser.get('/');
-    element(by.id('patternSearch')).sendKeys('Dell_1').submit();
-    expect(browser.getCurrentUrl()).toBe('http://127.0.0.1:8000/#!/logs?pattern=Dell_1');
-  });
-
-  it('visit /topics and /topics/:id:', function() {
-    browser.get('/#!/topics');
-    element.all(by.css('a.title')).first().click();
-    expect(browser.getCurrentUrl()).toMatch('/topics/[a-z0-9-]+$');
-  });
-
-  it('visit /job-definitions', function() {
-    browser.get('/#!/job-definitions');
-  });
-
-  it('visit /logs', function() {
-    browser.get('/#!/logs');
-  });
-
-  it('visit /information', function() {
-    browser.get('/#!/information');
-  });
-
-  it('visit /gpanel', function() {
-    browser.get('/#!/gpanel');
-  });
-
-  it('visit /admin/users', function() {
-    browser.get('/#!/admin/users');
-  });
-
-  it('visit /admin/teams', function() {
-    browser.get('/#!/admin/teams');
-  });
-
-  it('visit /admin/topics', function() {
-    browser.get('/#!/admin/topics');
-  });
-
-  it('visit /admin/remotecis', function() {
-    browser.get('/#!/admin/remotecis');
+  afterEach(function() {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 });
