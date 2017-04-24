ac22e0a19e5fb7363b047b96a75a485567bf5e93
Guillaume Vincent <gvincent@redhat.com>
2017-04-24 12:13:29 +0200

    Hide the menu link if the user does not have the rights
    Change-Id: Iabf3b259b3dbd35a76521072624740f3a6aecf6c

---
+++ src/js/components/admin/adminMenu.js
@@ -0,0 +1,76 @@
+// Copyright 2015 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+'use strict';
+
+require('app')
+  .component('adminMenu', {
+    templateUrl: '/partials/admin/menu.html',
+    controller: ['$state', 'auth', adminMenuCtrl],
+    bindings: {
+      endpoint: '@'
+    }
+  });
+
+function adminMenuCtrl($state, auth) {
+  var $ctrl = this;
+
+  $ctrl.tabs = [
+    {
+      title: "Users",
+      endpoint: "users",
+      state: "adminUsers",
+      superAdminRequired: false
+    },
+    {
+      title: "Teams",
+      endpoint: "teams",
+      state: "adminTeams",
+      superAdminRequired: true
+    },
+    {
+      title: "Topics",
+      endpoint: "topics",
+      state: "adminTopics",
+      superAdminRequired: true
+    },
+    {
+      title: "RemoteCIs",
+      endpoint: "remotecis",
+      state: "adminRemotecis",
+      superAdminRequired: false
+    },
+    {
+      title: "Audits",
+      endpoint: "audits",
+      state: "adminAudits",
+      superAdminRequired: false
+    }
+  ];
+
+  this.$onInit = function() {
+    $ctrl.activeTabIndex = 0;
+    $ctrl.isNotSuperAdmin = !auth.isAdmin();
+  };
+
+  $ctrl.tabs.forEach(function(tab, index) {
+    if (tab.endpoint === $ctrl.endpoint) {
+      $ctrl.activeTabIndex = index;
+    }
+  });
+
+  $ctrl.open = function(tab) {
+    $state.go(tab.state);
+  };
+}
