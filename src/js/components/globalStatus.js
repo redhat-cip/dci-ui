bdaceeb3222f034d3101257fd40a5b4e2b6f49a9
Guillaume Vincent <gvincent@redhat.com>
2017-05-10 21:58:48 +0200

    Refactor global status page
     * Use angular component
     * Create single page because topic detail can be see in /topics/:id: page
     * add some unit tests
    
    Change-Id: Ie5249f59c907cb008ee00e578ab12fdbdfe17478

---
+++ src/js/components/globalStatus.js
@@ -0,0 +1,56 @@
+// Copyright 2015 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+"use strict";
+
+require("app").component("globalStatus", {
+  templateUrl: "/partials/globalStatus.html",
+  controller: GlobalStatusCtrl,
+  bindings: {
+    topics: "="
+  }
+});
+
+function GlobalStatusCtrl() {
+  var $ctrl = this;
+
+  this.$onInit = function() {
+    var topicsOrder = {
+      OSP8: 1,
+      OSP9: 2,
+      OSP10: 3,
+      OSP11: 4,
+      OSP12: 5,
+      "RDO-Newton": 6,
+      "RDO-Ocata": 7,
+      "RDO-Pike": 8
+    };
+    _.each($ctrl.topics, function(topic) {
+      topic.success = 0;
+      topic.failures = 0;
+      topic.order = topicsOrder[topic.name] || 9;
+      _.each(topic.jobs, function(job) {
+        if (job.job_status === "success") {
+          topic.success += 1;
+        } else {
+          topic.failures += 1;
+        }
+      });
+      topic.percentageErrors = Math.round(
+        100 * topic.failures / (topic.success + topic.failures)
+      );
+    });
+    $ctrl.topics = _.sortBy($ctrl.topics, ["order"]);
+  };
+}
