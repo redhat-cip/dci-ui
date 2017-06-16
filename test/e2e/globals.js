da411cc5bc31c58d0d82edb844f20330309b314e
Guillaume Vincent <gvincent@redhat.com>
2017-06-16 09:24:34 +0200

    Replace protractor by nightwatch for faster end to end tests
     * remove selenium and use webdriver directly
     * remove deprecated phantomjs and use google-chrome headless mode
     * add login logout end to end tests
     * clean package.json and gulp
     * upgrade eslint
    
    Change-Id: I580cbd9340bcb35fec071f4faa76dae570ccf0a1

---
+++ test/e2e/globals.js
@@ -0,0 +1,33 @@
+// Copyright 2015 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+var chromedriver = require("chromedriver");
+var connect = require("connect");
+var serveStatic = require("serve-static");
+
+var server = connect().use(serveStatic("static")).listen(8686);
+
+module.exports = {
+  before: function(done) {
+    chromedriver.start();
+    done();
+  },
+
+  after: function(done) {
+    chromedriver.stop();
+    server.close(done);
+  },
+
+  waitForConditionTimeout: 30000
+};
