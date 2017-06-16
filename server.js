7058609b70d8bce460234ae4bcea87095374c3ef
Guillaume Vincent <gvincent@redhat.com>
2017-06-16 09:24:34 +0200

    Replace protractor by nightwatch for faster end to end tests
     * remove selenium and use webdriver directly
     * remove deprecated phantomjs and use google-chrome headless mode
     * add login logout end to end tests
     * clean package.json and gulp
     * upgrade eslint
    
    Change-Id: I580cbd9340bcb35fec071f4faa76dae570ccf0a1

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/static/index.html');
+});
+
+app.listen(8000, function() {
+  console.log('frontend listening on port 8000');
+});
