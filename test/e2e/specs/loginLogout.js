d4998716b55734e3085afb73b6120c0226fe9594
Guillaume Vincent <gvincent@redhat.com>
2017-06-16 09:24:34 +0200

    Replace protractor by nightwatch for faster end to end tests
     * remove selenium and use webdriver directly
     * remove deprecated phantomjs and use google-chrome headless mode
     * add login logout end to end tests
     * clean package.json and gulp
     * upgrade eslint
    
    Change-Id: I580cbd9340bcb35fec071f4faa76dae570ccf0a1

---
+++ test/e2e/specs/loginLogout.js
@@ -0,0 +1,46 @@
+// Copyright 2015 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+var assert = require("assert");
+
+module.exports = {
+  "Login logout tests": function(browser) {
+    browser.getLog(function(logs) {
+      console.log(JSON.stringify(logs, null, 2));
+    });
+
+    browser
+      .url(browser.launch_url)
+      .waitForElementVisible("#loginForm")
+      .setValue("input[type=text]", "user_dell")
+      .setValue("input[type=password]", "password")
+      .waitForElementVisible("button[type=submit]")
+      .click("button[type=submit]")
+      .waitForElementVisible("h1")
+      .assert.elementNotPresent('a[ui-sref="adminUsers"]')
+      .click("#btn-logout")
+      .setValue("input[type=text]", "admin_dell")
+      .setValue("input[type=password]", "password")
+      .waitForElementVisible("#loginForm")
+      .click("button[type=submit]")
+      .waitForElementVisible("h1")
+      .assert.elementPresent('a[ui-sref="adminUsers"]');
+
+    browser.getLog(function(logs) {
+      console.log(JSON.stringify(logs, null, 2));
+    });
+
+    browser.end();
+  }
+};
