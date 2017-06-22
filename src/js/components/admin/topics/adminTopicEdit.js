ace49a61f4fdc5b90e7d5e4fdeea835b003f4718
Guillaume Vincent <gvincent@redhat.com>
2017-06-22 12:34:15 +0200

    Fix Can't set Topic next_topic from the UI
    Change-Id: Iff1600f23f62fcc0259f887282c5a28c10ac8b78
    Fixes: #34
    Depends-On: I93ba1bf5923f87478a2f6fe68e5f007cb5f7be7d

--- src/js/components/admin/topics/adminTopicEdit.js
+++ src/js/components/admin/topics/adminTopicEdit.js
@@ -50,7 +50,7 @@ function adminTopicCtrl($state, api, messages) {
   $ctrl.update = function() {
     var topicName = $ctrl.topic.name;
     api.topics
-      .update($ctrl.topic)
+      .update(_.omitBy($ctrl.topic, _.isNull))
       .then(function() {
         messages.alert("topic " + topicName + " updated", "success");
         $state.go("adminTopics");
--- test/unit/admin/topics/adminTopicCreate.spec.js
+++ test/unit/admin/topics/adminTopicCreate.spec.js
@@ -12,14 +12,40 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-describe('admin topic create component', function() {
+describe("admin topic create component", function() {
   var component;
 
-  beforeEach(inject(function($componentController) {
-    component = $componentController('adminTopicCreate');
-  }));
+  beforeEach(
+    inject(function($componentController) {
+      component = $componentController("adminTopicCreate");
+    })
+  );
 
-  it('should init scope with empty topic', function() {
-    expect(component.topic.name).toBe('');
+  it("should init scope with empty topic", function() {
+    expect(component.topic.name).toBe("");
+    expect(component.topic.next_topic).toBe(null);
+  });
+
+  it("should create a topic", function() {
+    component.topic.name = "OSP10";
+    component.topic.next_topic = null;
+    $httpBackend
+      .expectPOST("https://api.example.org/api/v1/topics", { name: "OSP10" })
+      .respond();
+    component.create();
+    $httpBackend.flush();
+  });
+
+  it("should create a topic with next_topic", function() {
+    component.topic.name = "OSP11";
+    component.topic.next_topic = "4b1e3cae-41e0-409c-8a35-0af056c69525";
+    $httpBackend
+      .expectPOST("https://api.example.org/api/v1/topics", {
+        name: "OSP11",
+        next_topic: "4b1e3cae-41e0-409c-8a35-0af056c69525"
+      })
+      .respond();
+    component.create();
+    $httpBackend.flush();
   });
 });
