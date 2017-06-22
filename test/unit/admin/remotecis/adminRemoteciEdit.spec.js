5c2bb8ea8375f0175d9f465758c15744176edf50
Guillaume Vincent <gvincent@redhat.com>
2017-06-22 13:14:23 +0200

    Fix Can't set RemoteCI allow_upgrade_job from the UI
    Change-Id: Idd93279d756c60454001ff5f955290fd5fb26d6e
    Fixes: https://github.com/redhat-cip/dci-ui/issues/33

--- test/unit/admin/remotecis/adminRemoteciEdit.spec.js
+++ test/unit/admin/remotecis/adminRemoteciEdit.spec.js
@@ -12,71 +12,82 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-describe('admin remoteci edit component', function() {
+describe("admin remoteci edit component", function() {
   var component;
 
-  beforeEach(inject(function($componentController) {
-    component = $componentController('adminRemoteciEdit', null, {
-      remoteci: remoteci
-    });
-  }));
+  beforeEach(
+    inject(function($componentController) {
+      component = $componentController("adminRemoteciEdit", null, {
+        remoteci: remoteci
+      });
+    })
+  );
 
-  it('should init scope with prop remoteci', function() {
-    expect(component.remoteci.id).toBe('c76c40a2-82c7-40e8-959b-0883d32edda8');
-    expect(component.remoteci.name).toBe('Admin Remote CI');
-    expect(component.remoteci.state).toBe('active');
-    expect(component.remoteci.etag).toBe('48db63266779736d3c54d96946954a29');
+  it("should init scope with remoteci prop", function() {
+    expect(component.remoteci).toEqual(remoteci);
   });
 
-  it('should update if data are valid', function() {
+  it("should update if data are valid", function() {
     component.remoteci = {
-      id: '1',
-      name: 'rci',
-      state: 'active',
+      id: "1",
+      name: "rci",
+      state: "active",
+      allow_upgrade_job: true,
       data: {
-        "hardware": "Dell",
-        "network": "Juniper",
-        "storage": "swift",
-        "virtualization": "Xen"
+        hardware: "Dell",
+        network: "Juniper",
+        storage: "swift",
+        virtualization: "Xen"
       }
     };
 
     $httpBackend
-      .expectPUT('https://api.example.org/api/v1/remotecis/1', {
-        name: 'rci', state: 'active',
+      .expectPUT("https://api.example.org/api/v1/remotecis/1", {
+        name: "rci",
+        state: "active",
+        allow_upgrade_job: true,
         data: {
-          "hardware": "Dell",
-          "network": "Juniper",
-          "storage": "swift",
-          "virtualization": "Xen"
+          hardware: "Dell",
+          network: "Juniper",
+          storage: "swift",
+          virtualization: "Xen"
         }
       })
       .respond();
-    $httpBackend.whenGET('https://api.example.org/api/v1/remotecis').respond();
+    $httpBackend.whenGET("https://api.example.org/api/v1/remotecis").respond();
     component.update();
     $httpBackend.flush();
   });
 
-  it('should not update if data are invalid', function() {
+  it("should not update if data are invalid", function() {
     component.remoteci = {
-      id: '1',
-      name: 'rci',
-      state: 'active',
-      data: 'error'
+      id: "1",
+      name: "rci",
+      state: "active",
+      data: "error"
     };
     component.update();
   });
 
-  it('should refresh api secret', function() {
-    expect(component.remoteci.api_secret).toBe('736d3c54d96946954a2948db63266779');
+  it("should refresh api secret", function() {
+    expect(component.remoteci.api_secret).toBe(
+      "736d3c54d96946954a2948db63266779"
+    );
     $httpBackend
       .expectPUT(
-        'https://api.example.org/api/v1/remotecis/c76c40a2-82c7-40e8-959b-0883d32edda8/api_secret', {})
-      .respond({api_secret: '9db63266779736d54a29483c54d96946', etag: '54d96946'});
+        "https://api.example.org/api/v1/remotecis/c76c40a2-82c7-40e8-959b-0883d32edda8/api_secret",
+        {}
+      )
+      .respond({
+        api_secret: "9db63266779736d54a29483c54d96946",
+        etag: "54d96946"
+      });
     component.refreshApiSecretConfirmed();
     $httpBackend.flush();
-    expect(component.remoteci.api_secret).toBe('9db63266779736d54a29483c54d96946');
-    expect(component.remoteci.etag).toBe('54d96946');
-    expect(component.remoteci.id).toBe('c76c40a2-82c7-40e8-959b-0883d32edda8');
+    expect(component.remoteci.api_secret).toBe(
+      "9db63266779736d54a29483c54d96946"
+    );
+    expect(component.remoteci.etag).toBe("54d96946");
+    expect(component.remoteci.id).toBe("c76c40a2-82c7-40e8-959b-0883d32edda8");
   });
 });
