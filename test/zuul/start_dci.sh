95948b67a30ba8cc4713f675098aa386da268f5c
Guillaume Vincent <gvincent@redhat.com>
2017-09-01 16:51:41 +0200

    Fix end to end tests on Centos using last chromedriver version
    Change-Id: I508883b9032cf7cedc3fdb0e7e46bfa18a7c3f3c

--- test/zuul/start_dci.sh
+++ test/zuul/start_dci.sh
@@ -1,21 +1,20 @@
 set -ex
 
-# for all that script we assume that it as been launched from the root of this
-# repository and that https://github.com/redhat-cip/dci-control-server as been
-# cloned in a sibling folder i.e: ../dci-control-server
-
 export DCI_LOGIN=admin
 export DCI_PASSWORD=admin
-export DCI_CS_URL=http://localhost:5000
+export DCI_CS_URL=http://127.0.0.1:5000
 export DCI_SETTINGS_FILE="$(pwd)/test/zuul/settings.py"
-DCI_SERVER_DIR="../dci-control-server"
 
-cd "$DCI_SERVER_DIR"
+pushd "../dci-control-server"
 sudo "PATH=$PATH" pip install -r requirements.txt
 sudo "PATH=$PATH" python setup.py install
 sh scripts/start_db.sh
-sh scripts/start_es.sh
+sh scripts/start_api.sh
+sleep 10
 python scripts/db_provisioning.py -y
-python bin/dci-runtestserver &
-python bin/dci-esindex
-cd -
+popd
+npm install
+npm run build
+npm start &
+curl -u admin:admin http://127.0.0.1:5000/api/v1/jobs
+curl http://127.0.0.1:8000/
