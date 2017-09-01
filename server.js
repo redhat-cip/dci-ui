bdbd3d9a24ad396e4feae04cba42730e46bc9773
Guillaume Vincent <gvincent@redhat.com>
2017-09-01 16:51:41 +0200

    Fix end to end tests on Centos using last chromedriver version
    Change-Id: I508883b9032cf7cedc3fdb0e7e46bfa18a7c3f3c

--- server.js
+++ server.js
@@ -7,19 +7,6 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/static/index.html');
 });
 
-let _resolve;
-const readyPromise = new Promise(resolve => {
-  _resolve = resolve
-});
-
-const server = app.listen(8000, function() {
+app.listen(8000, function() {
   console.log('frontend listening on port 8000');
-  _resolve();
 });
-
-module.exports = {
-  ready: readyPromise,
-  close: () => {
-    server.close()
-  }
-};
