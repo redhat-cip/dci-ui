2c7b90bbbcd4974f7f3edd492df5bba54e29abad
Guillaume Vincent <gvincent@redhat.com>
2017-09-13 12:22:18 +0200

    Use new dbprovisioning script
    Change-Id: Id45facf5ccd63e3012dc9e40dc8a9d75fe09a28b
    Depends-On: Iadba94abaf6e1c31a2253b97073e5911d4bcfa88

--- test/zuul/start_dci.sh
+++ test/zuul/start_dci.sh
@@ -9,12 +9,12 @@ pushd "../dci-control-server"
 sudo "PATH=$PATH" python -m pip install -r requirements.txt
 sudo "PATH=$PATH" python setup.py install
 sh scripts/start_db.sh
-python scripts/db_provisioning.py -y
+python bin/dci-dbinit
 python bin/dci-runtestserver &
+python bin/dci-dbprovisioning
 popd
 npm install
 npm run build
 npm start &
-sleep 10
 curl -u admin:admin http://localhost:5000/api/v1/jobs
 curl http://localhost:8000/
