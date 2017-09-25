ef8ae1149ec8ada21156065ba5291ed358f36d3e
Guillaume Vincent <gvincent@redhat.com>
2017-09-25 13:24:54 +0200

    Make end to end tests patform agnostic
    Change-Id: Ie35187e9339b1b06958ec7e13e4dfc438eab3222

--- test/e2e/specs/noConsoleLogError.js
+++ test/e2e/specs/noConsoleLogError.js
@@ -1,4 +1,4 @@
-// Copyright 2015 Red Hat, Inc.
+// Copyright 2017 Red Hat, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the 'License'); you may
 // not use this file except in compliance with the License. You may obtain
@@ -12,70 +12,35 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-const assert = require("assert");
 const shortcuts = require("./shortcuts");
 
 module.exports = {
   "No console error test": function(browser) {
     shortcuts(browser)
       .login()
-      .goAndWaitH1("#navbar-utility__settings-link", "Update your settings")
-      .goAndWaitH1(
-        "#navbar-secondary__change-password-link",
-        "Change your password"
-      )
-      .goAndWaitH1(
-        "#navbar-secondary__notification-link",
-        "Subscribe to notifications"
-      )
-      .goAndWaitH1(
-        "#navbar-secondary__user-settings-link",
-        "Update your settings"
-      )
-      .goAndWaitH1("#navbar-primary__topics-link", "Topics")
-      .goAndWaitH1("#topics__create-topic-btn", "Create a new topic")
-      .goAndWaitH1(".btn-cancel", "Topics")
-      .go(".btn-edit")
-      .goAndWaitH1(".btn-cancel", "Topics")
-      .goAndWaitH1("#navbar-primary__components-link", "Components")
-      .goAndWaitH1("#navbar-primary__global-status-link", "Global Status")
-      .goAndWaitH1("#navbar-primary__metrics-link", "Metrics")
-      .goAndWaitH1("#navbar-primary__admin-users-link", "Users")
-      .goAndWaitH1("#admin__create-user-btn", "Create a new user")
-      .goAndWaitH1(".btn-cancel", "Users")
-      .go(".btn-edit")
-      .goAndWaitH1(".btn-cancel", "Users")
-      .goAndWaitH1("#navbar-secondary__admin-teams-link", "Teams")
-      .goAndWaitH1("#admin__create-team-btn", "Create a new team")
-      .goAndWaitH1(".btn-cancel", "Teams")
-      .go(".btn-edit")
-      .goAndWaitH1(".btn-cancel", "Teams")
-      .goAndWaitH1("#navbar-secondary__admin-remotecis-link", "Remotecis")
-      .goAndWaitH1("#admin__create-remoteci-btn", "Create a new remoteci")
-      .goAndWaitH1(".btn-cancel", "Remotecis")
-      .go(".btn-edit")
-      .goAndWaitH1(".btn-cancel", "Remotecis")
-      .goAndWaitH1("#navbar-secondary__admin-products-link", "Products")
-      .goAndWaitH1("#admin__create-product-btn", "Create a new product")
-      .goAndWaitH1(".btn-cancel", "Products")
-      .go(".btn-edit")
-      .goAndWaitH1(".btn-cancel", "Products")
-      .goAndWaitH1("#navbar-secondary__admin-users-link", "Users");
-
-    browser.getLog(function(logs) {
-      logs = logs.filter(function(log) {
-        return (
-          ["DEBUG", "INFO", "WARNING"].indexOf(log.level) === -1 &&
-          log.source !== "network"
-        );
-      });
-      assert(
-        logs.length === 0,
-        "Console log error(s):\n" + JSON.stringify(logs, null, 2)
-      );
-    });
-
-    shortcuts(browser)
+      .clearLogs()
+      .go("settings")
+      .checkLogs("Settings")
+      .go("changePassword")
+      .checkLogs("Change password")
+      .go("notification")
+      .checkLogs("Notification")
+      .go("topics")
+      .checkLogs("Topics")
+      .go("components")
+      .checkLogs("Components")
+      .go("globalStatus")
+      .checkLogs("Global Status")
+      .go("metrics")
+      .checkLogs("Metrics")
+      .go("adminUsers")
+      .checkLogs("Admin Users")
+      .go("adminTeams")
+      .checkLogs("Admin Teams")
+      .go("adminRemotecis")
+      .checkLogs("Admin Remotecis")
+      .go("adminProducts")
+      .checkLogs("Admin Products")
       .logout()
       .end();
   }
--- test/e2e/specs/shortcuts.js
+++ test/e2e/specs/shortcuts.js
@@ -1,53 +1,152 @@
+// Copyright 2017 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+const assert = require("assert");
+
 module.exports = function(browser) {
-  return {
-    goAndWaitH1(id, h1) {
-      browser
-        .waitForElementVisible(id)
-        .click(id)
-        .useXpath()
-        .waitForElementVisible(`//h1[normalize-space(text())="${h1}"]`)
-        .useCss();
-      return this;
-    },
-    go(id) {
-      browser.waitForElementVisible(id).click(id);
-      return this;
-    },
-    changePassword(currentPassword, newPassword) {
-      browser
-        .waitForElementVisible("#current_password")
-        .setValue("#current_password", currentPassword)
-        .setValue("#new_password", newPassword)
-        .setValue("#new_password2", newPassword)
-        .waitForElementVisible("#changePasswordButton")
-        .click("#changePasswordButton");
-      return this;
-    },
-    login(user = "admin", password = "admin") {
-      browser
-        .url(browser.launch_url)
-        .waitForElementVisible("#inputUsername")
-        .pause(1000)
-        .setValue("#inputUsername", user)
-        .setValue("#inputPassword", password)
-        .waitForElementVisible("#logInButton")
-        .click("#logInButton")
-        .waitForElementVisible("job-summary")
-        .pause(300)
-        .waitForElementVisible("h1")
-        .assert.containsText("h1", "Dashboard");
-      return this;
-    },
-    logout() {
-      browser
-        .waitForElementVisible("#navbar-utility__logout-link")
-        .click("#navbar-utility__logout-link")
-        .pause(1000)
-        .refresh();
-      return this;
-    },
-    end() {
-      browser.end();
-    }
+  browser.login = function(user = "admin", password = "admin") {
+    this.url(browser.launch_url)
+      .waitForElementVisible("#inputUsername")
+      .pause(1000)
+      .clearValue("#inputUsername")
+      .clearValue("#inputPassword")
+      .setValue("#inputUsername", user)
+      .setValue("#inputPassword", password)
+      .waitForElementVisible("#logInButton")
+      .click("#logInButton")
+      .waitForElementVisible("job-summary")
+      .pause(300)
+      .waitForElementVisible("h1")
+      .assert.containsText("h1", "Dashboard");
+    return this;
   };
+
+  browser.logout = function() {
+    this.waitForElementVisible("#navbar-utility__logout-link")
+      .click("#navbar-utility__logout-link")
+      .pause(1000)
+      .refresh();
+    return this;
+  };
+
+  browser.changePassword = function(currentPassword, newPassword) {
+    this.waitForElementVisible("#new_password2")
+      .clearValue("#current_password")
+      .clearValue("#new_password")
+      .clearValue("#new_password2")
+      .setValue("#current_password", currentPassword)
+      .setValue("#new_password", newPassword)
+      .setValue("#new_password2", newPassword)
+      .waitForElementVisible("button#changePasswordButton")
+      .click("button#changePasswordButton");
+    return this;
+  };
+
+  browser.waitForXpathElementVisible = function(element) {
+    this.useXpath()
+      .waitForElementVisible(element)
+      .useCss();
+    return this;
+  };
+
+  browser.checkLogs = function(page) {
+    this.getLog(logs => {
+      const formattedLogs = JSON.stringify(logs, null, 2);
+      assert(
+        logs.length === 0,
+        `Console log error in ${page} page: ${formattedLogs}`
+      );
+    });
+    return this;
+  };
+
+  browser.clearLogs = function() {
+    this.getLog(() => {});
+    return this;
+  };
+
+  browser.go = function(page) {
+    switch (page) {
+      case "jobs":
+        return this.click(
+          "#navbar-primary__jobs-link"
+        ).waitForXpathElementVisible(
+          "//span[normalize-space(text())='RH7-RHOS-12.0 2016-11-12.2']"
+        );
+      case "topics":
+        return this.click(
+          "#navbar-primary__topics-link"
+        ).waitForXpathElementVisible("//a[normalize-space(text())='OSP10']");
+      case "createTopic":
+        return this.click("#navbar-primary__topics-link")
+          .waitForXpathElementVisible("//a[normalize-space(text())='OSP10']")
+          .click("#topics__create-topic-btn")
+          .waitForElementVisible("button#createButton");
+      case "components":
+        return this.click(
+          "#navbar-primary__components-link"
+        ).waitForXpathElementVisible(
+          "//span[normalize-space(text())='RH7-RHOS-10.0 2016-10-28.1']"
+        );
+      case "globalStatus":
+        return this.click(
+          "#navbar-primary__global-status-link"
+        ).waitForXpathElementVisible("//a[normalize-space(text())='OSP11']");
+      case "metrics":
+        return this.click(
+          "#navbar-primary__metrics-link"
+        ).waitForXpathElementVisible("//a[normalize-space(text())='3 Months']");
+      case "adminUsers":
+        return this.click(
+          "#navbar-primary__admin-users-link"
+        ).waitForXpathElementVisible("//a[normalize-space(text())='user_hp']");
+      case "createUser":
+        return this.click("#navbar-primary__admin-users-link")
+          .waitForXpathElementVisible("//a[normalize-space(text())='user_hp']")
+          .click("#admin__create-user-btn")
+          .waitForElementVisible("button#createButton");
+      case "adminTeams":
+        return this.click(
+          "#navbar-secondary__admin-teams-link"
+        ).waitForXpathElementVisible(
+          "//a[normalize-space(text())='OpenStack']"
+        );
+      case "adminRemotecis":
+        return this.click(
+          "#navbar-secondary__admin-remotecis-link"
+        ).waitForXpathElementVisible(
+          "//a[normalize-space(text())='Remoteci OpenStack']"
+        );
+      case "adminProducts":
+        return this.click(
+          "#navbar-secondary__admin-products-link"
+        ).waitForXpathElementVisible("//a[normalize-space(text())='Ansible']");
+      case "settings":
+        return this.click(
+          "#navbar-utility__settings-link"
+        ).waitForElementVisible("button#changeSettingsButton");
+      case "changePassword":
+        return this.click("#navbar-utility__settings-link")
+          .click("#navbar-secondary__change-password-link")
+          .waitForElementVisible("button#changePasswordButton");
+      case "notification":
+        return this.click(
+          "#navbar-secondary__notification-link"
+        ).waitForElementVisible("i.fa-plus");
+    }
+    return this;
+  };
+
+  return browser;
 };
