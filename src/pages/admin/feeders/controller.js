d18b46ce182ef0f47fba5da4b5e322d0476c9d20
Yanis Guenane <yguenane@redhat.com>
2017-10-12 16:08:50 +0200

    Feeder: Introduce the feeder notion
    A new resource has been introduced: feeders.
    
    This commit allows one to manage this feeder resource.
    It allows all the CRUD operations on it for SUPER_ADMIN and
    PRODUCT_OWNER.
    
    Change-Id: I377e6d67ca7844170a9dac8ca5e443304dc0f5e4
    Depends-On: I2d282d2688e761b76ca77e75cba0cf559e52b8c2

---
+++ src/pages/admin/feeders/controller.js
@@ -0,0 +1,72 @@
+// Copyright 2017 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import api from "services/api";
+import * as alertsActions from "services/alerts/actions";
+import FileSaver from "file-saver";
+
+class Ctrl {
+  constructor($scope, $ngRedux, $uibModal) {
+    this.$ngRedux = $ngRedux;
+    this.$uibModal = $uibModal;
+    let unsubscribe = $ngRedux.connect(state => state)(this);
+    $scope.$on("$destroy", unsubscribe);
+  }
+
+  $onInit() {
+    this.loading = true;
+    this.$ngRedux.dispatch(api("feeder").all({ embed: "team" })).then(() => {
+      this.loading = false;
+    });
+  }
+
+  toggleLockFeeder(feeder) {
+    const newFeeder = Object.assign({}, feeder);
+    if (newFeeder.state === "active") {
+      newFeeder.state = "inactive";
+    } else {
+      newFeeder.state = "active";
+    }
+    this.$ngRedux.dispatch(api("feeder").put(newFeeder));
+  }
+
+  deleteFeeder(feeder) {
+    const feederName = feeder.name;
+    const deleteFeederModal = this.$uibModal.open({
+      component: "confirmDestructiveAction",
+      resolve: {
+        data: function() {
+          return {
+            title: "Delete feeder " + feederName,
+            body: "Are you you want to delete feeder " + feederName + "?",
+            okButton: "Yes delete " + feederName,
+            cancelButton: "oups no!"
+          };
+        }
+      }
+    });
+    deleteFeederModal.result.then(() => {
+      this.$ngRedux.dispatch(api("feeder").delete(feeder)).then(() => {
+        this.$ngRedux.dispatch(
+          alertsActions.success(`feeder deleted successfully`)
+        );
+      });
+    });
+  }
+
+}
+
+Ctrl.$inject = ["$scope", "$ngRedux", "$uibModal"];
+
+export default Ctrl;
