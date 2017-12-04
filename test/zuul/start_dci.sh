0592945c5b4b3eef6cd7f14f14443db29a85a566
Guillaume Vincent <gvincent@redhat.com>
2017-12-04 17:49:53 +0100

    wip
    Change-Id: I68ab0161ab2c28b192e618040032a38b73432d2c

--- test/zuul/start_dci.sh
+++ test/zuul/start_dci.sh
@@ -1,21 +1,9 @@
 set -ex
-
-export DCI_LOGIN=admin
-export DCI_PASSWORD=admin
-export DCI_CS_URL=http://localhost:5000
-export DCI_SETTINGS_FILE="$(pwd)/test/zuul/settings.py"
-
 pushd "../dci-control-server"
-sudo "PATH=$PATH" python -m pip install -r requirements.txt
-sudo "PATH=$PATH" python setup.py install
 sh scripts/start_db.sh
-python bin/dci-dbinit
-python bin/dci-runtestserver &
+sh scripts/start_api.sh
 python bin/dci-dbprovisioning
 popd
 npm install
 npm run build
 npm start &
-sleep 10
-curl -u admin:admin http://localhost:5000/api/v1/jobs
-curl http://localhost:8000/
