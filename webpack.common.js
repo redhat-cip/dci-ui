f0bb0fcfba36f4d19693eaaf77979bc5b7456f94
Guillaume Vincent <gvincent@redhat.com>
2018-05-26 12:06:43 +0200

    Webpack v4 migration
    Change-Id: Icc7359880f880afc75be467e2351299a4d85341f

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,11 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: {
-    app: "./src/app.js",
-    vendor: "./src/vendor.js"
-  },
+  entry: "./src/app.js",
   resolve: {
     modules: [path.resolve(__dirname, "src"), "node_modules"]
   },
@@ -19,47 +15,33 @@ module.exports = {
       template: "./src/index.html",
       inject: "body"
     }),
-    new ExtractTextPlugin("[name].[contenthash].css"),
     new CopyWebpackPlugin([{ context: "./src", from: "config.json", to: "" }]),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new webpack.WatchIgnorePlugin(["./src/config.json"])
   ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules\/(?!copy-text-to-clipboard)/,
-        loader: "babel-loader"
-      },
-      {
-        test: /(\.css|\.scss)$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader!sass-loader",
-          publicPath: ""
-        })
-      },
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader!less-loader",
-          publicPath: ""
-        })
-      },
-      { test: /\.html$/, loader: "raw-loader" },
-      {
-        test: /\.(png|jpg|jpeg|gif|ico)$/,
-        loader: "file-loader?name=[name].[ext]"
-      },
-      {
-        test: /\.(woff|woff2|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=[name].[ext]"
-      }
-    ]
-  },
   output: {
     filename: "[name].[chunkhash].js",
     path: path.resolve(__dirname, "static")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        use: ["file-loader?name=[name].[ext]"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      { test: /\.html$/, loader: "raw-loader" }
+    ]
   }
 };
