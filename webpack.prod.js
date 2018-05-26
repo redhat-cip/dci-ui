ec0b2eaf0b11511caca4bd6a1d3cc51f91209f73
Guillaume Vincent <gvincent@redhat.com>
2018-05-26 12:06:43 +0200

    Webpack v4 migration
    Change-Id: Icc7359880f880afc75be467e2351299a4d85341f

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,33 +4,14 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
+  mode: "production",
   devtool: "source-map",
   plugins: [
-    new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
     new UglifyJSPlugin({
-      sourceMap: true,
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: false
-      },
-      compress: {
-        screw_ie8: true
-      },
-      comments: false
+      sourceMap: true
     }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: "vendor"
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: "runtime"
-      }),
-      new webpack.HashedModuleIdsPlugin(),
+    })
   ]
 });
