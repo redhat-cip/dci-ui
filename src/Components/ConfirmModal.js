53b011000c4955b51d1adee1b7101d4bcab97ba4
Guillaume Vincent <gvincent@redhat.com>
2018-06-22 17:12:45 +0200

    transform team edit, team delete modal and team creation into react components
    Change-Id: I10e3494c5e07b67d82525fb68e23a5433f73a92b

---
+++ src/Components/ConfirmModal.js
@@ -0,0 +1,67 @@
+// Copyright 2017 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import React from "react";
+import PropTypes from "prop-types";
+import { Modal, Icon, Button } from "patternfly-react";
+
+export default class ConfirmModal extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Modal show={this.props.show} onHide={this.props.close}>
+          <Modal.Header>
+            <button
+              className="close"
+              onClick={this.props.close}
+              aria-hidden="true"
+              aria-label="Close"
+            >
+              <Icon type="pf" name="close" />
+            </button>
+            <Modal.Title>{this.props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{this.props.children}</Modal.Body>
+          <Modal.Footer>
+            <Button
+              bsStyle="default"
+              className="btn-cancel"
+              onClick={this.props.close}
+            >
+              {this.props.cancelButton}
+            </Button>
+            <Button bsStyle="danger" onClick={this.props.onValidSubmit}>
+              {this.props.okButton}
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
+
+ConfirmModal.propTypes = {
+  title: PropTypes.string.isRequired,
+  okButton: PropTypes.string,
+  cancelButton: PropTypes.string,
+  show: PropTypes.bool,
+  close: PropTypes.func,
+  onValidSubmit: PropTypes.func.isRequired
+};
+
+ConfirmModal.defaultProps = {
+  show: false,
+  okButton: "ok",
+  cancelButton: "cancel"
+};
---
+++ src/Components/Teams/EditTeamButton.js
@@ -0,0 +1,49 @@
+// Copyright 2017 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+import React from "react";
+import { connect } from "../../store";
+import TeamForm from "./TeamForm";
+import actions from "./actions";
+
+export class EditTeamForm extends React.Component {
+  render() {
+    return (
+      <TeamForm
+        title="Edit team"
+        team={this.props.team}
+        teams={this.props.teams}
+        showModalButton={<i className="fa fa-pencil" />}
+        okButton="Edit"
+        submit={team => {
+          const newTeam = {
+            id: this.props.team.id,
+            ...team
+          };
+          this.props.editTeam(newTeam);
+        }}
+      />
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    editTeam: team => dispatch(actions.update(team))
+  };
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(EditTeamForm);
