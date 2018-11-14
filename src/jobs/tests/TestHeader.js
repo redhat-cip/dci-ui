58e003c796bd811ccf8c447c9260eea3790660bf
Guillaume Vincent <gvincent@redhat.com>
2018-11-14 10:06:59 +0100

    Use labels in tests page
    Change-Id: Id4d0ce7ea2c966d0ecf42a140f71bfcdb857605d

--- src/jobs/tests/TestHeader.js
+++ src/jobs/tests/TestHeader.js
@@ -6,47 +6,52 @@ export default class TestHeader extends Component {
   render() {
     const { test, toggleDetails } = this.props;
     return (
-      <h2 className="page-header">
-        <span className="pf-u-mr-xl">{test.name || "Test"}</span>
-        <small className="pf-u-mr-xl">(Duration: {test.time} msec)</small>
-        <small>
-          <span className="label label-primary pf-u-mr-xl">
-            {test.total} tests
-          </span>
-          {test.success ? (
-            <span className="label label-success pf-u-mr-xl">
-              {test.success} success
-            </span>
-          ) : null}
-          {test.skips ? (
-            <span className="label label-warning pf-u-mr-xl">
-              {test.skips} skipped
-            </span>
-          ) : null}
-          {test.errors ? (
-            <span className="label label-default pf-u-mr-xl">
-              {test.errors} errors
-            </span>
-          ) : null}
-          {test.failures ? (
-            <span className="label label-danger pf-u-mr-xl">
-              {test.failures} failures
-            </span>
-          ) : null}
-          {test.regressions ? (
-            <Labels.Regression>
-              {test.regressions} regressions
-            </Labels.Regression>
-          ) : null}
-        </small>
-        <Button
-          type="button"
-          variant="tertiary"
-          onClick={() => toggleDetails()}
-        >
-          See tests details
-        </Button>
-      </h2>
+      <div className="pf-c-data-list__item pf-u-mb-sm">
+        <div className="pf-c-data-list__cell">
+          {test.name || "Test"} (Duration: {test.time} msec)
+        </div>
+        <div className="pf-c-data-list__cell">
+          <small>
+            <Labels.Default className="pf-u-mr-xs">
+              {test.total} tests
+            </Labels.Default>
+            {test.success ? (
+              <Labels.Success className="pf-u-mr-xs">
+                {test.success} success
+              </Labels.Success>
+            ) : null}
+            {test.skips ? (
+              <Labels.Warning className="pf-u-mr-xs">
+                {test.skips} skipped
+              </Labels.Warning>
+            ) : null}
+            {test.errors ? (
+              <Labels.Error className="pf-u-mr-xs">
+                {test.errors} errors
+              </Labels.Error>
+            ) : null}
+            {test.failures ? (
+              <Labels.Failure className="pf-u-mr-xs">
+                {test.failures} failures
+              </Labels.Failure>
+            ) : null}
+            {test.regressions ? (
+              <Labels.Regression>
+                {test.regressions} regressions
+              </Labels.Regression>
+            ) : null}
+          </small>
+        </div>
+        <div className="pf-c-data-list__cell pf-u-text-align-right">
+          <Button
+            type="button"
+            variant="tertiary"
+            onClick={() => toggleDetails()}
+          >
+            See tests details
+          </Button>
+        </div>
+      </div>
     );
   }
 }
