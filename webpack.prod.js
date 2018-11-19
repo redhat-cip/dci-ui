6c33f982bb86098420711c196509305644d9f13b
Guillaume Vincent <gvincent@redhat.com>
2018-11-19 17:56:25 +0100

    Fix rpm build failing
    Update node modules to fix error in dci-rpm-build
    
    Change-Id: I4baa55da678898a771b084aae2d0d214ede0cf01

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,14 @@
-const webpack = require("webpack");
 const merge = require("webpack-merge");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].[hash].css"
     })
   ]
 });
