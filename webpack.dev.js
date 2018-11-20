0045e7accdedece0181ad31895bcfab77cbbcdb1
Guillaume Vincent <gvincent@redhat.com>
2018-11-20 12:05:00 +0100

    Fix rpm build failing
    Change-Id: I1531bef9ae5ca5b9b939d7d450a9e10e77182b45

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,8 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
+    host: "0.0.0.0",
+    port: 8000,
     contentBase: "./build",
     historyApiFallback: true
   }
