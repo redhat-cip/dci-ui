c5378b7860748ba3c9d3380c63ef9407138dc26b
Guillaume Vincent <gvincent@redhat.com>
2018-11-20 12:05:00 +0100

    Fix rpm build failing
    Change-Id: I1531bef9ae5ca5b9b939d7d450a9e10e77182b45

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,9 @@
-const webpack = require("webpack");
 const merge = require("webpack-merge");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
-    })
-  ]
+  optimization: { minimizer: [new TerserPlugin()] }
 });
