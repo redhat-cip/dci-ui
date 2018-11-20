f4f9981eef4e437d1c127111cb8717418e79391b
Guillaume Vincent <gvincent@redhat.com>
2018-11-20 15:16:17 +0100

    Clean webpack configuration files
    Change-Id: I705230aa5658d625cd7cfbda9c5b714efc602f60

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,24 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-  optimization: { minimizer: [new TerserPlugin()] }
+  optimization: { minimizer: [new TerserPlugin()] },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  }
 });
