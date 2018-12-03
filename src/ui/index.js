bf8f59188e23afb87fd25c042f21fd74e9a74da9
Guillaume Vincent <gvincent@redhat.com>
2018-12-03 17:47:03 +0100

    Use patternfly-react css only
    Change-Id: Ie571bc18e8be7645279fc0e33e464a7ef3c29652

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,6 +1,5 @@
 export { default as Colors } from "./Colors";
 export { default as BlinkLogo } from "./blinkLogo/BlinkLogo";
-export { default as Cover } from "./cover/Cover";
 export { default as EmptyState } from "./EmptyState";
 export { default as Labels } from "./Labels";
 export { default as CopyButton } from "./CopyButton";
