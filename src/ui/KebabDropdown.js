c7e5414c9aa9ab82b94aa4f8199f6a4ed1eefc4e
Guillaume Vincent <gvincent@redhat.com>
2018-12-03 17:47:03 +0100

    Use patternfly-react css only
    Change-Id: Ie571bc18e8be7645279fc0e33e464a7ef3c29652

---
+++ src/ui/KebabDropdown.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { KebabToggle, Dropdown } from "@patternfly/react-core";
+
+export default class KebabDropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  onToggle = isOpen => {
+    this.setState({
+      isOpen
+    });
+  };
+
+  onSelect = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }));
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    const { items, ...props } = this.props;
+    return (
+      <Dropdown
+        onToggle={this.onToggle}
+        onSelect={this.onSelect}
+        toggle={<KebabToggle onToggle={this.onToggle} />}
+        isOpen={isOpen}
+        isPlain
+        dropdownItems={items}
+        {...props}
+      />
+    );
+  }
+}
