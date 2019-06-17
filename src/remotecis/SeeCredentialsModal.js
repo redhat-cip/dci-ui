6bbb2de7fb6fdc35ee1f8eb00079592fdbb5f4e5
Guillaume Vincent <gvincent@redhat.com>
2019-06-17 09:36:50 +0200

    Replace rcom file with displayed modal
    When a partner configure his remoteci, it's easier to copy paste the content of the rcom file.
    
    Change-Id: I0cc0457da98104572a4cea9e345201448c87c5df

---
+++ src/remotecis/SeeCredentialsModal.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Button, Modal } from "@patternfly/react-core";
+import { UserSecretIcon } from "@patternfly/react-icons";
+import copyToClipboard from "../services/copyToClipboard";
+
+export default class SeeCredentialsModal extends Component {
+  state = {
+    show: false,
+    copied: false
+  };
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  closeModal = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    const { remoteci } = this.props;
+    const { show, copied } = this.state;
+
+    const credentials = `
+DCI_CLIENT_ID='remoteci/${remoteci.id}' 
+DCI_API_SECRET='${remoteci.api_secret}'
+DCI_CS_URL='https://api.distributed-ci.io/' 
+export DCI_CLIENT_ID
+export DCI_API_SECRET
+export DCI_CS_URL
+`;
+
+    return (
+      <React.Fragment>
+        <Modal
+          title={`DCI credentials for ${remoteci.name}`}
+          isOpen={show}
+          onClose={this.closeModal}
+          isLarge
+          actions={[
+            <Button key="cancel" variant="secondary" onClick={this.closeModal}>
+              close
+            </Button>,
+            <Button
+              key="copy"
+              onClick={event => {
+                copyToClipboard(event, credentials);
+                this.setState({ copied: true });
+              }}
+            >
+              {copied ? "Copied!" : "Copy to clipboard"}
+            </Button>
+          ]}
+        >
+          <pre>{credentials}</pre>
+        </Modal>
+        <Button onClick={this.showModal}>
+          <UserSecretIcon />
+        </Button>
+      </React.Fragment>
+    );
+  }
+}
