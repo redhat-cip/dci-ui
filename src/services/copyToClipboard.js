0c4d368100c53f66ffcacf4aa9d0b42a039d13f9
Guillaume Vincent <gvincent@redhat.com>
2019-06-19 17:00:57 +0200

    Fix copy to clipboard removing line break
    Change-Id: I4efa2e138dd48da69f280da4581510010c490eb1

--- src/services/copyToClipboard.js
+++ src/services/copyToClipboard.js
@@ -1,7 +1,7 @@
 export default function copyToClipboard(event, text) {
   const clipboard = event.currentTarget.parentElement;
-  const el = document.createElement("input");
-  el.value = text;
+  const el = document.createElement("textarea");
+  el.innerHTML = text;
   clipboard.appendChild(el);
   el.select();
   document.execCommand("copy");
