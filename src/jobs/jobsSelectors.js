263aeb0a24b896067560bb745a527d3e1ede9857
Guillaume Vincent <gvincent@redhat.com>
2019-08-02 09:06:23 +0200

    Use job duration from control server
    Change-Id: If469c98127cc1af677ae1b5f7bceb2f3ca87c11b
    Depends-On: Ifad5b04d30e1dece3f96e7163de34e446fef0300

--- src/jobs/jobsSelectors.js
+++ src/jobs/jobsSelectors.js
@@ -3,6 +3,8 @@ import { sortBy } from "lodash";
 import { getTeamsById } from "teams/teamsSelectors";
 import { getRemotecisById } from "remotecis/remotecisSelectors";
 import { getTopicsById } from "topics/topicsSelectors";
+import { getTimezone } from "currentUser/currentUserSelectors";
+import { formatDate, humanizeDuration } from "services/date";
 
 export const getJobsById = state => state.jobs.byId;
 export const getJobsAllIds = state => state.jobs.allIds;
@@ -10,9 +12,10 @@ export const getJobs = createSelector(
   getRemotecisById,
   getTopicsById,
   getTeamsById,
+  getTimezone,
   getJobsById,
   getJobsAllIds,
-  (remotecis, topics, teams, jobs, jobsAllIds) =>
+  (remotecis, topics, teams, currentUserTimezone, jobs, jobsAllIds) =>
     sortBy(
       jobsAllIds.map(id => {
         const job = jobs[id];
@@ -20,7 +23,9 @@ export const getJobs = createSelector(
           ...job,
           team: teams[job.team_id],
           topic: topics[job.topic_id],
-          remoteci: remotecis[job.remoteci_id]
+          remoteci: remotecis[job.remoteci_id],
+          datetime: formatDate(job.created_at, currentUserTimezone),
+          humanizedDuration: humanizeDuration(job.duration)
         };
       }),
       [job => new Date(job.created_at)]
