8aec2a58f28713e0ffe0f95853463e7d1b5e37d0
Guillaume Vincent <gvincent@redhat.com>
2019-11-13 18:18:16 +0100

    Refactor trends pages
    c3 and react-c3js are just wrapper over d3.
    Remove c3 and use d3 directly.
    Display all graph in the trends page.
    Focus only on successful jobs from partners.
    Allow details when a user click on a specific topic.
    
    Change-Id: I1d25253806c4c93882bbb0117589119834348130

--- src/trends/TrendGraph.js
+++ src/trends/TrendGraph.js
@@ -1,78 +1,87 @@
 import React, { Component } from "react";
-import C3Chart from "react-c3js";
-import moment from "moment";
-import { isEmpty } from "lodash";
-import { EmptyState } from "ui";
-import { Card, CardHeader, CardBody } from "@patternfly/react-core";
+import * as d3 from "d3";
 
-export default class TrendGraph extends Component {
+class TrendGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.graph = React.createRef();
+  }
+  componentDidMount() {
+    const { data, xMin, xMax } = this.props;
+    this.drawGraph(this.graph, data, xMin, xMax);
+  }
+  drawGraph = (element, data, xMin, xMax) => {
+    const graphWidth = element.current.offsetWidth;
+    const isWide = graphWidth > 780;
+    const marginMultiplier = isWide ? 3 : 1;
+    const margin = {
+      top: 15 * marginMultiplier,
+      right: 15 * marginMultiplier,
+      bottom: 15 * marginMultiplier,
+      left: 15 * marginMultiplier
+    };
+    const width = graphWidth - margin.left - margin.right;
+    const graphHeight = Math.round((width * 1) / 2);
+    const height = graphHeight - margin.top - margin.bottom;
+
+    const svg = d3
+      .select(element.current)
+      .append("svg")
+      .attr("width", graphWidth)
+      .attr("height", graphHeight)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    if (!data) return;
+
+    const dataOrdered = data.map(d => [d3.timeParse("%s")(d[0]), d[1], d[2]]);
+
+    const x = d3
+      .scaleTime()
+      .domain([xMin, xMax])
+      .range([0, width]);
+    if (isWide) {
+      svg
+        .append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+    }
+    const yMin = 0;
+    const dataY1 = dataOrdered.map(d => d[1]);
+    const y1Max = d3.max(dataY1);
+
+    const y = d3
+      .scaleLinear()
+      .domain([yMin, y1Max])
+      .range([height, 0]);
+
+    if (isWide) {
+      const yAxisTicks = y.ticks().filter(tick => Number.isInteger(tick));
+      const yAxis = d3
+        .axisLeft(y)
+        .tickValues(yAxisTicks)
+        .tickFormat(d3.format("d"));
+      svg.append("g").call(yAxis);
+    }
+
+    const barWidth = isWide ? 5 : 2;
+    const strokeWidth = isWide ? 1 : 0;
+    svg
+      .selectAll("rect")
+      .data(dataOrdered)
+      .enter()
+      .append("rect")
+      .attr("fill", "#92D400")
+      .attr("stroke", "#486B00")
+      .attr("stroke-width", strokeWidth)
+      .attr("x", d => x(d[0]))
+      .attr("y", d => y(d[1]))
+      .attr("height", d => height - y(d[1]))
+      .attr("width", barWidth);
+  };
   render() {
-    const { trend, topic } = this.props;
-    if (isEmpty(trend))
-      return (
-        <EmptyState
-          title="No stat"
-          info={`There is no stats for topic ${topic.name}.`}
-        />
-      );
-    const rows = trend.reduce(
-      (acc, trend) => {
-        acc.push([trend[0] * 1000, trend[1], trend[2]]);
-        return acc;
-      },
-      [["x", "success", "failures"]]
-    );
-    const xDaysAgoUnix =
-      moment()
-        .subtract(90, "days")
-        .unix() * 1000;
-    const nowUnix = moment().unix() * 1000;
-    return (
-      <Card>
-        <CardHeader>
-          {topic.name} job in success or failure for each day
-        </CardHeader>
-        <CardBody>
-          <C3Chart
-            data={{
-              x: "x",
-              xFormat: null,
-              rows: rows,
-              types: {
-                success: "area-spline",
-                failures: "area-spline"
-              },
-              groups: [["success", "failures"]],
-              colors: {
-                success: "#307628",
-                failures: "#a30000"
-              }
-            }}
-            point={{
-              show: false
-            }}
-            zoom={{
-              enabled: true,
-              rescale: true
-            }}
-            axis={{
-              y: { show: true, label: topic.name },
-              x: {
-                type: "timeseries",
-                tick: {
-                  format: "%b %d %Y",
-                  outer: false,
-                  fit: true
-                },
-                extent: [xDaysAgoUnix, nowUnix]
-              }
-            }}
-            subchart={{
-              show: true
-            }}
-          />
-        </CardBody>
-      </Card>
-    );
+    return <div ref={this.graph}></div>;
   }
 }
+
+export default TrendGraph;
