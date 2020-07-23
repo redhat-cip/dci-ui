de94f9437a0ff9c19ff6b10ccabb3152c9389195
Guillaume Vincent <gvincent@redhat.com>
2020-07-23 07:59:56 +0200

    Transform EmailsFilter to typescript
    Change-Id: I66375c6018b2189161017dc49914ffc9da48efc4

---
+++ src/users/EmailsFilter.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {
+  Button,
+  InputGroup,
+  TextInput,
+  ButtonVariant,
+  ToolbarFilter,
+} from "@patternfly/react-core";
+import { SearchIcon } from "@patternfly/react-icons";
+
+type EmailsFilterProps = {
+  search: string;
+  onSearch: (search: string) => void;
+  onClear: () => void;
+};
+
+const EmailsFilter = ({ search, onSearch, onClear }: EmailsFilterProps) => {
+  const [email, setEmail] = useState("");
+  return (
+    <ToolbarFilter
+      chips={search ? [search] : []}
+      deleteChip={onClear}
+      categoryName="Email"
+      showToolbarItem
+    >
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setEmail("");
+          onSearch(email);
+        }}
+      >
+        <InputGroup>
+          <TextInput
+            name="email"
+            id="input-email"
+            type="email"
+            aria-label="email filter"
+            onChange={(email) => setEmail(email)}
+            value={email}
+            placeholder="Filter by email..."
+          />
+          <Button
+            variant={ButtonVariant.control}
+            aria-label="search email button"
+            type="submit"
+          >
+            <SearchIcon />
+          </Button>
+        </InputGroup>
+      </form>
+    </ToolbarFilter>
+  );
+};
+
+export default EmailsFilter;
