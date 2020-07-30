753f85f89759f38884f25f3dbef5ee232fe8b8f2
Guillaume Vincent <gvincent@redhat.com>
2020-07-30 15:02:58 +0200

    Display date with browser timezone
    Change-Id: Ic4b00854b847fdf030fd004700ac3bd5ed77ac05

--- src/services/date.js
+++ src/services/date.js
@@ -1,16 +1,24 @@
 import { DateTime } from "luxon";
 import humanize from "humanize-duration";
 
-export function fromNow(dateString) {
-  return DateTime.fromISO(dateString).toRelative();
+export function fromNow(
+  dateString,
+  timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+) {
+  return DateTime.fromISO(dateString, { zone: "utc" })
+    .setZone(timezone)
+    .toRelative();
 }
 
 export function humanizeDuration(durationInMs) {
   return humanize(durationInMs, { round: true, largest: 1 });
 }
 
-export function formatDate(datetime) {
-  return DateTime.fromISO(datetime).toLocaleString(
-    DateTime.DATETIME_MED_WITH_SECONDS
-  );
+export function formatDate(
+  datetime,
+  timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+) {
+  return DateTime.fromISO(datetime, { zone: "utc" })
+    .setZone(timezone)
+    .toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
 }
