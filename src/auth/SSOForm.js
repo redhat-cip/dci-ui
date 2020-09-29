84c8c2cde29842b08a292be6fceffc80a87f022b
Guillaume Vincent <gvincent@redhat.com>
2020-09-29 15:05:03 +0200

    Display an error message when SSO is down
    Change-Id: Ic1c8654b54ba977e983151df3916d88ff58a489b

--- src/auth/SSOForm.js
+++ src/auth/SSOForm.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Button, ActionGroup } from "@patternfly/react-core";
 import { useAuth } from "./authContext";
 import { useLocation } from "react-router-dom";
+import { showError } from "alerts/alertsActions";
+import { useDispatch } from "react-redux";
 
 const SSOForm = () => {
   const { sso } = useAuth();
   const location = useLocation();
+  const dispatch = useDispatch();
   return (
     <div>
       <ActionGroup>
@@ -13,7 +16,13 @@ const SSOForm = () => {
           variant="danger"
           className="mt-md"
           onClick={() => {
-            sso.signinRedirect({ state: location.state });
+            sso.signinRedirect({ state: location.state }).catch(() => {
+              dispatch(
+                showError(
+                  "Oops, we are sorry! We can't connect to sso.redhat.com. Can you try later ?"
+                )
+              );
+            });
           }}
         >
           Log in
