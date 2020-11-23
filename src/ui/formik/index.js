a38c7949681f2aeb034ab634843040e23f4e5efd
Guillaume Vincent <gvincent@redhat.com>
2020-11-23 08:45:49 +0100

    Replace AddUserToTeamsForm into Typescript
    Change-Id: Icc55bf84298dd9051934177460c8e87ff19e518a

--- src/ui/formik/index.js
+++ src/ui/formik/index.js
@@ -2,3 +2,4 @@ export { default as Input } from "./Input";
 export { default as Checkbox } from "./Checkbox";
 export { default as TextArea } from "./TextArea";
 export { default as FormModal } from "./FormModal";
+export { default as SelectWithSearch } from "./SelectWithSearch";
