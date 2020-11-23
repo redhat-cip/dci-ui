f8de9090817895f8f5054be0c88febbd546ead56
Guillaume Vincent <gvincent@redhat.com>
2020-11-23 08:45:49 +0100

    Replace AddUserToTeamsForm into Typescript
    Change-Id: Icc55bf84298dd9051934177460c8e87ff19e518a

---
+++ src/ui/formik/SelectWithSearch.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useField } from "formik";
+import {
+  FormGroup,
+  Select,
+  SelectVariant,
+  SelectOption,
+} from "@patternfly/react-core";
+
+type SelectOptionType = {
+  label: string;
+  value: string;
+};
+
+interface DCISelectProps2 {
+  id: string;
+  label?: string;
+  placeholder?: string;
+  name: string;
+  options: SelectOptionType[];
+  [x: string]: any;
+}
+
+export default function DCISelect({
+  id,
+  label,
+  placeholder,
+  name,
+  options,
+  ...props
+}: DCISelectProps2) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [field, meta, helpers] = useField(name);
+  const { setValue } = helpers;
+  const validated = meta.touched && meta.error ? "error" : "default";
+  return (
+    <FormGroup
+      label={label}
+      isRequired={props.isRequired}
+      fieldId={id}
+      helperTextInvalid={meta.error}
+      validated={validated}
+    >
+      <Select
+        {...field}
+        {...props}
+        variant={SelectVariant.typeahead}
+        typeAheadAriaLabel={placeholder}
+        onToggle={setIsOpen}
+        onSelect={(event, selection) => {
+          const s = selection as SelectOptionType;
+          setIsOpen(false);
+          setValue(s.value);
+        }}
+        onClear={() => setValue(null)}
+        selections={
+          field.value ? options.find((o) => o.value === field.value)?.label : ""
+        }
+        isOpen={isOpen}
+        aria-labelledby="select"
+        placeholderText={placeholder}
+      >
+        {options
+          .map((o) => ({ ...o, toString: () => o.label }))
+          .map((option, i) => (
+            <SelectOption key={i} value={option} />
+          ))}
+      </Select>
+    </FormGroup>
+  );
+}
---
+++ src/users/AddUserToTeamsForm.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Button, Flex, FlexItem } from "@patternfly/react-core";
+import { SelectWithSearch } from "ui/formik";
+import * as Yup from "yup";
+import { ITeam } from "types";
+import { AppDispatch } from "store";
+import { Formik, Form } from "formik";
+import teamsActions from "teams/teamsActions";
+
+interface AddUserToTeamFormProps {
+  onSubmit: (team: ITeam) => void;
+}
+
+const AddUserToTeamSchema = Yup.object().shape({
+  team_id: Yup.string().nullable().required("Team is required"),
+});
+
+export default function AddUserToTeamForm({
+  onSubmit,
+}: AddUserToTeamFormProps) {
+  const dispatch = useDispatch<AppDispatch>();
+  const [teams, setTeams] = useState<ITeam[]>([]);
+
+  useEffect(() => {
+    dispatch(teamsActions.all()).then((response) => {
+      setTeams(response.data.teams);
+    });
+  }, [dispatch]);
+
+  return (
+    <Formik
+      initialValues={{ team_id: "" }}
+      validationSchema={AddUserToTeamSchema}
+      onSubmit={(v) => {
+        const selectedTeam = teams.find((t) => t.id === v.team_id);
+        if (selectedTeam) {
+          onSubmit(selectedTeam);
+        }
+      }}
+    >
+      {({ isValid, dirty }) => (
+        <Form id="add_user_to_team_form" className="pf-c-form">
+          <div>
+            <Flex>
+              <FlexItem>Add user in</FlexItem>
+              <FlexItem>
+                <SelectWithSearch
+                  id="add_user_to_team_formadd_user_to_team_form__team_id"
+                  name="team_id"
+                  option={""}
+                  options={teams.map((t) => ({ label: t.name, value: t.id }))}
+                />
+              </FlexItem>
+              <FlexItem>team</FlexItem>
+              <FlexItem>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  isDisabled={!(isValid && dirty)}
+                >
+                  Add
+                </Button>
+              </FlexItem>
+            </Flex>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+}
