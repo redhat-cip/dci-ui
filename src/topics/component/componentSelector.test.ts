511dde8dc763636dc47d862767b43177a4919867
Guillaume Vincent <gvincent@redhat.com>
2025-03-19 20:31:22 +0100

    Fix getPrincipalComponent
    getPrincipalComponent returns openshift-preflight component. This is wrong.
    A component with openshift in the name without the type ocp should not be returned.
    
    This patch fixes the problem.
    
    Change-Id: I87f6737d93d2e62765aed791e9208d6f11ab4dee

--- src/topics/component/componentSelector.test.ts
+++ src/topics/component/componentSelector.test.ts
@@ -3,20 +3,23 @@ import { getPrincipalComponent } from "./componentSelector";
 
 test("getPrincipalComponent OpenShift", () => {
   const components = [
-    { id: "c1", display_name: "OpenShift 4.8.35" },
+    { id: "c1", display_name: "OpenShift 4.8.35", type: "ocp" },
     {
       id: "c2",
-      display_name: "python3-openshift 0.11.2-1.el8",
+      display_name: "python3-dciclient 4.0.1-1.202503121102git6074135f.el8",
+      type: "rpm",
     },
   ] as IComponent[];
 
   expect(getPrincipalComponent(components)).toEqual({
     id: "c1",
     display_name: "OpenShift 4.8.35",
+    type: "ocp",
   });
   expect(getPrincipalComponent(components.reverse())).toEqual({
     id: "c1",
     display_name: "OpenShift 4.8.35",
+    type: "ocp",
   });
 });
 
@@ -24,17 +27,20 @@ test("getPrincipalComponent RHOS", () => {
   const components = [
     {
       id: "c1",
-      display_name: "16.2-RHEL-8",
+      display_name: "RHOS-17.1-RHEL-9-20250214.n.0",
+      type: "compose",
     },
     {
       id: "c2",
-      display_name: "python3-swift 0.11.2-1.el8",
+      display_name: "python3-dciclient 4.0.1-1.202503121102git6074135f.el8",
+      type: "rpm",
     },
   ] as IComponent[];
 
   expect(getPrincipalComponent(components)).toEqual({
     id: "c1",
-    display_name: "16.2-RHEL-8",
+    display_name: "RHOS-17.1-RHEL-9-20250214.n.0",
+    type: "compose",
   });
 });
 
@@ -43,16 +49,19 @@ test("getPrincipalComponent RHEL", () => {
     {
       id: "c1",
       display_name: "RHEL-8.2.0-20220311.1",
+      type: "compose",
     },
     {
       id: "c2",
-      display_name: "python3-swift 0.11.2-1.el8",
+      display_name: "python3-dciclient 4.0.1-1.202503121102git6074135f.el8",
+      type: "rpm",
     },
   ] as IComponent[];
 
   expect(getPrincipalComponent(components)).toEqual({
     id: "c1",
     display_name: "RHEL-8.2.0-20220311.1",
+    type: "compose",
   });
 });
 
@@ -61,11 +70,35 @@ test("getPrincipalComponent Microshift", () => {
     {
       id: "c1",
       display_name: "Microshift 4.16.0 ec.2",
+      type: "repo",
+    },
+    {
+      id: "c2",
+      display_name: "python3-dciclient 4.0.1-1.202503121102git6074135f.el8",
+      type: "rpm",
     },
   ] as IComponent[];
 
   expect(getPrincipalComponent(components)).toEqual({
     id: "c1",
     display_name: "Microshift 4.16.0 ec.2",
+    type: "repo",
+  });
+});
+
+test("nrt getPrincipalComponent doesn't returns other openshift component", () => {
+  const components = [
+    {
+      id: "c2",
+      display_name: "openshift-preflight beebfaf",
+      type: "openshift-preflight",
+    },
+    { id: "c1", display_name: "OpenShift 4.8.35", type: "ocp" },
+  ] as IComponent[];
+
+  expect(getPrincipalComponent(components)).toEqual({
+    id: "c1",
+    display_name: "OpenShift 4.8.35",
+    type: "ocp",
   });
 });
--- src/topics/component/componentSelector.ts
+++ src/topics/component/componentSelector.ts
@@ -1,11 +1,11 @@
-export function getPrincipalComponent<T extends { display_name: string }>(
-  components: T[],
-) {
+export function getPrincipalComponent<
+  T extends { display_name: string; type: string },
+>(components: T[]) {
   return (
     components.find((component) => {
       const name = component.display_name.toLowerCase();
       return (
-        /^openshift/.test(name) ||
+        (/^openshift/.test(name) && component.type === "ocp") ||
         /^microshift/.test(name) ||
         name.indexOf("rhel-") !== -1 ||
         name.indexOf("rhos-") !== -1
